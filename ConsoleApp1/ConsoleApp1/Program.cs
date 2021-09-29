836afcd09c6f7007a5a29c56b07a254864713ddb
Juan Fdez. Montojo <jpfmontojo@gmail.com>
2021-09-29 15:09:49 +0300

    Edited code in VS

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,8 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             
             // Coded changed and edited in GitHub
+
+            // Code added in Visual Studio
         }
     }
 }
